16e4b3ef9b6666d400e2631568f18800b79a8fda
Malwina Czepiżak <malwina.czepizak@gmail.com>
2020-10-10 21:22:52 +0200

    Refactoring in App component

--- src/layouts/App/index.js
+++ src/layouts/App/index.js
@@ -1,51 +1,53 @@
 import React, {useState} from 'react';
 import './App.scss';
-// import {HashRouter as Router, Route, Switch} from "react-router-dom";
+
 
 import DiceGenerator from "../../components/DiceSelector/DiceGenerator";
 import ShowResult from "../../components/ResultView/ShowResult";
 import SearchButton from "../../components/Search/SearchButton";
 
 function App() {
-   const [throwData, setThrowData] = useState({
-       type: null,
-       amount: 1,
-       modifier: 0
-   })
+    const [throwData, setThrowData] = useState({
+        type: null,
+        amount: 1,
+        modifier: 0
+    })
+    const [allRolls, setAllRolls] = useState([]);
 
-  const [allRolls, setAllRolls] =  useState([]);
+    const handleRoll = () => {
+        fetch(`https://rolz.org/api/?${Number(throwData.amount)}d${throwData.type}.json`)
+            .then(response => response.json())
+            .then(response => {
+                let formatReturn = response.details.replace(/['('|')' ]/g, '').split("+")
+                setAllRolls(prevState => [{response: formatReturn, data: throwData}, ...prevState])
+            });
+    }
 
-  const handleRoll = () => {
-      fetch(`https://rolz.org/api/?${Number(throwData.amount)}d${throwData.type}.json`)
-          .then(response => response.json())
-          .then(response => {
-              let formatReturn = response.details.replace(/['('|')' ]/g,'').split("+")
-              setAllRolls(prevState=>[{response:formatReturn, data:throwData},...prevState])
-          });
-  }
+    const handleClear = () => {
+        setThrowData({
+            type: null,
+            amount: 1,
+            modifier: 0
+        })
+    }
 
-  const handleClear = () => {
-      setThrowData({
-          type: null,
-          amount: 1,
-          modifier: 0
-      })
-  }
+    return (
+        <div className={"roller"}>
+            <section className={"roller__dices"}>
+                <div>
 
-  return (
-    // <Router>
-    //   <Switch>
-    //     <Route exact path="/" component={DiceGenerator}/>
-    //   </Switch>
-    // </Router>
-      <>
-          <DiceGenerator onCollect={setThrowData} throwData={throwData} />
-          <button onClick={handleRoll}>Roll</button>
-          <button onClick={handleClear}>Clear</button>
-          <ShowResult allRolls={allRolls} setAllRolls={setAllRolls} />
-          <SearchButton />
-      </>
-  );
+
+                <DiceGenerator onCollect={setThrowData} throwData={throwData}/>
+                <button onClick={handleRoll}>Roll</button>
+                <button onClick={handleClear}>Clear</button>
+                </div>
+                <SearchButton/>
+            </section>
+            <section className={"roller__results"}>
+                <ShowResult allRolls={allRolls} setAllRolls={setAllRolls}/>
+            </section>
+        </div>
+    );
 }
 
 export default App;
