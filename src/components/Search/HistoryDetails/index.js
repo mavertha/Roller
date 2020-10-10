bda6430220f67b2da27593c634251925a2454c15
Malwina Czepiżak <malwina.czepizak@gmail.com>
2020-10-10 22:08:05 +0200

    Refactoring in App component cont.

--- src/components/Search/HistoryDetails/index.js
+++ src/components/Search/HistoryDetails/index.js
@@ -1,23 +1,28 @@
 import React from "react";
 import ShowHistoryRolls from "../ShowHistoryRolls";
 
-export default function HistoryDetails( {allHistory} ) {
+export default function HistoryDetails( {allHistory,onClose} ) {
     return (
         <>
-            <ul>
-                {allHistory.map((item, i) => {
-                    return (
-                        <li key={i}>
-                            <h3>
-                                {item.date} {item.name}
-                            </h3>
-                            {item.entry.map((data, i) => (
-                                <ShowHistoryRolls key={i} {...data} />
-                            ))}
-                        </li>
-                    );
-                })}
-            </ul>
+            <h1>History results</h1>
+            <div>
+                <ul>
+                    {allHistory.map((item, i) => {
+                        return (
+                            <li key={i}>
+                                <h3>
+                                    {item.date} {item.name}
+                                </h3>
+                                {item.entry.map((data, i) => (
+                                    <ShowHistoryRolls key={i} {...data} />
+                                ))}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <button onClick={()=>onClose(false)}>close history</button>
+
         </>
     )
 }
--- src/layouts/App/index.js
+++ src/layouts/App/index.js
@@ -1,10 +1,10 @@
 import React, {useState} from 'react';
 import './App.scss';
 
-
 import DiceGenerator from "../../components/DiceSelector/DiceGenerator";
 import ShowResult from "../../components/ResultView/ShowResult";
-import SearchButton from "../../components/Search/SearchButton";
+
+import HistoryDetails from "../../components/Search/HistoryDetails";
 
 function App() {
     const [throwData, setThrowData] = useState({
@@ -13,7 +13,35 @@ function App() {
         modifier: 0
     })
     const [allRolls, setAllRolls] = useState([]);
+    const [showForm, setShowForm] = useState(false);
+    const [date, setDate] = useState("");
+    const [text, setText] = useState("");
+    const dataFromLocalStorage = JSON.parse(localStorage.getItem('history'));
+    const [allHistory, setAllHistory] = useState([...dataFromLocalStorage]);
+    const [showHistoryDetails, setShowHistoryDetails] = useState(false);
 
+    const handleSearch = () => {
+        let filteredHistory = allHistory.filter((item) => {
+            if (date === "") {
+                return item.name.includes(text);
+            } else if (text === "") {
+                return item.date === date;
+            } else {
+                return item.name.includes(text) && item.date === date;
+            }
+        });
+        setAllHistory([...filteredHistory]);
+        setShowHistoryDetails(true);
+    }
+
+    const handleClearHistoryForm = () => {
+        setDate("");
+        setText("");
+        setAllHistory([...dataFromLocalStorage]);
+    }
+    const handleShow = () => {
+        setShowForm(true);
+    }
     const handleRoll = () => {
         fetch(`https://rolz.org/api/?${Number(throwData.amount)}d${throwData.type}.json`)
             .then(response => response.json())
@@ -23,7 +51,7 @@ function App() {
             });
     }
 
-    const handleClear = () => {
+    const handleClearSelection = () => {
         setThrowData({
             type: null,
             amount: 1,
@@ -35,16 +63,34 @@ function App() {
         <div className={"roller"}>
             <section className={"roller__dices"}>
                 <div>
-
-
-                <DiceGenerator onCollect={setThrowData} throwData={throwData}/>
-                <button onClick={handleRoll}>Roll</button>
-                <button onClick={handleClear}>Clear</button>
+                    <DiceGenerator onCollect={setThrowData} throwData={throwData}/>
+                    <button onClick={handleRoll}>Roll</button>
+                    <button onClick={handleClearSelection}>Clear</button>
                 </div>
-                <SearchButton/>
+                <div>
+                    <button onClick={handleShow}>Search for previous results</button>
+                    {showForm && <div>
+
+
+                        <input
+                            type="date"
+                            value={date}
+                            onChange={(e) => setDate(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            value={text}
+                            onChange={(e) => setText(e.target.value)}
+                        />
+                        <button onClick={handleSearch}>Search</button>
+                        <button onClick={handleClearHistoryForm}>Clear</button>
+                    </div>}
+                </div>
+
             </section>
             <section className={"roller__results"}>
-                <ShowResult allRolls={allRolls} setAllRolls={setAllRolls}/>
+                {!showHistoryDetails&& <ShowResult allRolls={allRolls} setAllRolls={setAllRolls}/>}
+                {showHistoryDetails && <HistoryDetails allHistory={allHistory} onClose={setShowHistoryDetails} />}
             </section>
         </div>
     );
