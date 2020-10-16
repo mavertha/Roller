de0749790130a770f4243a2f5c975deb6720b4df
Malwina Czepiżak <malwina.czepizak@gmail.com>
2020-10-16 19:41:13 +0200

    Added conditional rendering

--- src/components/Search/HistoryDetails/index.js
+++ src/components/Search/HistoryDetails/index.js
@@ -11,26 +11,40 @@ export default function HistoryDetails( {allHistory, onClose, setDisallowSearch,
         setAllHistory([...dataFromLocalStorage]);
     }
 
-    return (
-        <div className="roller__results__container">
-            <div className="roller__frame">
-                <h2>History results</h2>
+    if (allHistory.length === 0) {
+        return (
+            <div className="roller__results__container">
+                <div className="roller__frame">
+                    <h2>History results</h2>
+                </div>
+                <div className="roller__results__view">
+                    <p className="roller__nohistory__alert">Nothing has been found</p>
+                </div>
+                <button className="history__results__btn" onClick={handleClearHistory}>Close history</button>
             </div>
-            <div className="roller__results__view">
-                {allHistory.map((item, i) => {
-                    return (
-                        <li key={i}>
-                            <h3>
-                                {item.date} - {item.name}
-                            </h3>
-                            {item.entry.map((data, i) => (
-                                <ShowHistoryRolls key={i} {...data} />
+        )
+    } else {
+        return (
+            <div className="roller__results__container">
+                <div className="roller__frame">
+                    <h2>History results</h2>
+                </div>
+                <div className="roller__results__view">
+                    {allHistory.map((item, i) => {
+                        return (
+                            <li key={i}>
+                                <h3>
+                                    {item.date} - {item.name}
+                                </h3>
+                                {item.entry.map((data, i) => (
+                                    <ShowHistoryRolls key={i} {...data} />
                                 ))}
-                        </li>
-                    );
-                })}
+                            </li>
+                        );
+                    })}
+                </div>
+                <button className="history__results__btn" onClick={handleClearHistory}>Close history</button>
             </div>
-            <button className="history__results__btn" onClick={handleClearHistory}>Close history</button>
-        </div>
-    )
+        )
+    }
 }
--- src/components/Search/ShowHistoryRolls/index.js
+++ src/components/Search/ShowHistoryRolls/index.js
@@ -3,10 +3,6 @@ import React from "react";
 const ShowHistoryRolls = ({ data, response }) => {
     return (
         <p className="roller__single__result">
-            {/*<h3>Było rzucane d{data.type}</h3>*/}
-            {/*{response.join(",")} Suma + modyfikator:*/}
-            {/*{response.reduce((acc, curr) => acc + Number(curr), Number(data.modifier))}*/}
-
             <span className="text__shadow__span">Dice:</span><span className={`dice__type__d${data.type}`}>D{data.type}</span><br/>
             <span className="text__shadow__span">Throws:</span><span>{response.join("  ")}</span><br/>
             <span className="text__shadow__span">Total:</span>{response.reduce((acc, curr) => acc + Number(curr), Number(data.modifier))}
