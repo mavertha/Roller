6fe6d94d72f418a229916691fd4265b031420fcf
Malwina Czepiżak <malwina.czepizak@gmail.com>
2021-09-07 19:09:10 +0200

    Changed animation name

--- src/components/DiceSelector/Dices/index.js
+++ src/components/DiceSelector/Dices/index.js
@@ -1,40 +1,31 @@
 import React, {useState} from "react";
 import "./Dices.scss";
 
-const Dice = ({ type, onCollect }) => {
+const Dice = ({type, onCollect}) => {
     const [newClass, setNewClass] = useState("");
-
-    const handleClick = () => {
-        onCollect(prevState=>({...prevState, type}));
-        setNewClass('roller__animation');
-        console.log("!!!!!")
-    }
-
     return (
         <div className="dice"
-        //     onClick={() => {
-        //     onCollect(prevState=>({...prevState, type}));
-        //     setNewClass('roller__animation');
-        // }}
-            onClick={handleClick}
-            onAnimationEnd={() => setNewClass("")}>
-            <div className={`dice-image dice-d${type} ${newClass}`} />
+             onClick={() => {
+                 onCollect(prevState => ({...prevState, type}));
+                 setNewClass('roller__animation');
+             }}
+             onAnimationEnd={() => setNewClass("")}>
+            <div className={`dice-image dice-d${type} ${newClass}`}/>
             <h2>d{type}</h2>
         </div>
     );
 };
 
-export default function SelectDice( {onCollect} ) {
-
+export default function SelectDice({onCollect}) {
     return (
-            <div className="roller__generator__dices">
-                <Dice type="4" onCollect={onCollect}/>
-                <Dice type="6" onCollect={onCollect}/>
-                <Dice type="8" onCollect={onCollect}/>
-                <Dice type="10" onCollect={onCollect}/>
-                <Dice type="12" onCollect={onCollect}/>
-                <Dice type="20" onCollect={onCollect}/>
-                <Dice type="100" onCollect={onCollect}/>
-            </div>
+        <div className="roller__generator__dices">
+            <Dice type="4" onCollect={onCollect}/>
+            <Dice type="6" onCollect={onCollect}/>
+            <Dice type="8" onCollect={onCollect}/>
+            <Dice type="10" onCollect={onCollect}/>
+            <Dice type="12" onCollect={onCollect}/>
+            <Dice type="20" onCollect={onCollect}/>
+            <Dice type="100" onCollect={onCollect}/>
+        </div>
     );
 };
